582d32c05b27f75bbf5900dbc611b9e27a8ad90f
Alejandro <amendez1361@bths.edu>
2026-01-28 19:52:11 -0500

    Added evaluate player button

---
+++ App.js
@@ -0,0 +1,12 @@
+// reserved space for importing fetchPlayer function
+// reserved space for importing normalizePlayerStats function
+// reserved space for importing evaluatePlayer function
+
+// main app
+
+// select a player by searching name
+// after stats are fetched, confirm player
+ <button onClick={evaluatePlayer}>
+        Evaluate Player
+      </button>
+// display results page
